2537508081baa6e8da43d1a6896409151ed6c97d
mverteuil <onceuponajooks@gmail.com>
2012-08-06 10:53:35 -0400

    Added categories, getting entries, writing them to the console

--- starslurper/slurper.py
+++ starslurper/slurper.py
@@ -4,14 +4,32 @@ Toronto Star News Grabber and Formatter
 """
 from feedreader import parser
 
-RSS_FEED = "http://www.thestar.com/rss?category=%2fnews"
+
+RSS_TEMPLATE = "http://www.thestar.com/rss?category=/%s"
+RSS_CATEGORIES = [
+    "news",
+    "travel",
+    "business",
+    "opinion",
+    "news/sciencetech",
+    "sports",
+    "entertainment",
+]
+
+
+def get_articles():
+    """ Get articles for all categories and print them to the console """
+    for category in RSS_CATEGORIES:
+        feed_url = RSS_TEMPLATE % category
+        feed = parser.from_url(feed_url)
+        print "%s: %s" % ( str(category).upper(), feed.title )
+        for entry in feed.entries:
+            print entry.id
 
 
 def main():
-    response = parser.from_url(RSS_FEED)
-    print response.title
-    print response.link
-    print response.published
+    get_articles()
+
 
 if __name__ == "__main__":
     main()
