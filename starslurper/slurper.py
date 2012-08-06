8b5fc64f42af3f47264966b3ae50a8bcb2d0af31
mverteuil <onceuponajooks@gmail.com>
2012-08-06 11:22:25 -0400

    Converts RSS feed URLs to print view URLs, using a generator for get_articles since that makes more sense.
    Probably ready to pull down the print view stuff and start
    parsing/converting it soon.

--- starslurper/slurper.py
+++ starslurper/slurper.py
@@ -2,9 +2,13 @@
 """
 Toronto Star News Grabber and Formatter
 """
+import re
+
 from feedreader import parser
 
 
+token_matcher = re.compile(r"/(\d+)--")
+PRINT_TEMPLATE = "http://www.thestar.com/printarticle/%s"
 RSS_TEMPLATE = "http://www.thestar.com/rss?category=/%s"
 RSS_CATEGORIES = [
     "news",
@@ -17,18 +21,29 @@ RSS_CATEGORIES = [
 ]
 
 
+def convert_to_print_view(url):
+    """ Converts article URL from the default browser view to the minimal 
+    print version """
+    url = str(url) # Sometimes this isn't a string proper and blows up
+    match = token_matcher.search(url)
+    token = match.groups()[0]
+    return PRINT_TEMPLATE % token
+
+
 def get_articles():
-    """ Get articles for all categories and print them to the console """
+    """ Get articles for all categories and return them for processing """
     for category in RSS_CATEGORIES:
         feed_url = RSS_TEMPLATE % category
         feed = parser.from_url(feed_url)
-        print "%s: %s" % ( str(category).upper(), feed.title )
-        for entry in feed.entries:
-            print entry.id
+        articles = [convert_to_print_view(article.id) for article in feed.entries]
+        yield (category, articles)
 
 
 def main():
-    get_articles()
+    for category, articles in get_articles():
+        print category
+        for article in articles:
+            print article
 
 
 if __name__ == "__main__":
